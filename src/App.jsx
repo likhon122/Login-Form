c1238162cac8a35e60035da348864aa7c83c5986
Likhon Islam <md.likhonislam2x@gmail.com>
2023-10-03 17:19:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./index.css"
+import Resister from "./components/feature/resistrationPage/resister";
+
+function App() {
+  return (
+    <div>
+      <Resister/>
+    </div>
+  );
+}
+
+export default App;
